dada19399488ff4a14f7b745811558b475af4407
codybinu <binayak.bidyasagar99@gmail.com>
2023-09-10 00:04:29 +0530

    1ST day react portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./component/navbar/navbar";
+import Intro from "./component/intro/intro"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Intro/>
+    </div>
+  );
+}
+
+export default App;
